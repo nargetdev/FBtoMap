c998079dd9c58fedf229748bec933c2b42fa9911
Akshay Jetli <ajetli@umich.edu>
2014-10-11 21:29:58 -0400

    Finished distance.js except it is untested. 'Pretty print' might also be messed up since I used a standard javascript function to use.

--- distance.js
+++ distance.js
@@ -7,9 +7,30 @@
 	Number.prototype.prettyPrint = function() {
 		// return a string representation of an integer
 		// which has commas every three digits
+
+		var val = this.valueOf();
+		val.toLocaleString();
+		return val;
 	};
 
 	distanceFormula = function(first, second){
 		// compute the distance in miles between two lat lng points
 		// a point should look like {lat: 0.0, lng: 0.0}
+
+		var R = 3963;
+		//This is the radius of the earth in miles.
+
+		if(!first | !second) return 0;
+
+		var dLat = (second.lat - first.lat).toRad();
+		var dLng = (second.lng - first.lng).toRad();
+		var lat1 = first.lat.toRad();
+		var lat2 = second.lat.toRad();
+
+		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+				Math.sin(dLng/2) * Math.sin(dLng/2) * Math.cos(lat1) * Math.cos(lat2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		var d =  R * c;
+
+		return d;
 	};
